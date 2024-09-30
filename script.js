910cea9786fbe6a5fce452c2887a6de334cc5556
sujipinku07 <sujipinku07@gmail.com>
2024-09-30 19:16:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const input=document.getElementById('fname');
+function capitalize(){
+	const currentvalue=input.value;
+	input.value=currentvalue.toUpperCase();
+}
+input.addEventListener('blur',capitalize)
